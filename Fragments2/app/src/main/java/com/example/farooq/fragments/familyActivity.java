3f1cecd1ed63e7d58c462696830fff613f70f6c8
mudassirAfzal <mua\addsi@gmail.com>
2016-12-11 01:40:33 +0500

    miwok added in recent times

---
+++ Fragments2/app/src/main/java/com/example/farooq/fragments/familyActivity.java
@@ -0,0 +1,13 @@
+package com.example.farooq.fragments;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class familyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_family);
+    }
+}
---
+++ Fragments2/app/src/main/java/com/example/farooq/fragments/numberActivity.java
@@ -0,0 +1,13 @@
+package com.example.farooq.fragments;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class numberActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_number);
+    }
+}
